5a2883632e8b1cb65bd1a0840e873b5eff9582dd
Facundo Torres <facutor@gmail.com>
2023-08-24 13:52:14 -0300

    js

--- Git-Modulo-3-Unidad-3/js/mayor.js
+++ Git-Modulo-3-Unidad-3/js/mayor.js
@@ -1,13 +1,25 @@
 
 function encontrarMayor(arr) {
-    return Math.max(...arr);
+    let mayor = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > mayor) {
+            mayor = arr[i];
+        }
+    }
+
+    return mayor;
 }
 
 function mostrarMayor() {
     const inputNumeros = document.getElementById("numeros");
     const resultado = document.getElementById("resultado2");
 
-    const numerosArray = inputNumeros.value.split(",").map(Number);
+    const numerosArray = inputNumeros.value.split(",");
+    for (let i = 0; i < numerosArray.length; i++) {
+        numerosArray[i] = parseFloat(numerosArray[i]);
+    }
+
     const mayor = encontrarMayor(numerosArray);
 
     resultado.textContent = `El número mayor es: ${mayor}`;
