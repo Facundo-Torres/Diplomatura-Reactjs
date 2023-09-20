4636a715e341e9a0112a6d8a31e6fb512b52f177
Facundo Torres <facutor@gmail.com>
2023-09-20 15:05:24 -0300

    node

--- Modulo-4-Node/routes/empresa.js
+++ Modulo-4-Node/routes/empresa.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('Hola Empresa');
 });
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
 
 module.exports = router;
  
