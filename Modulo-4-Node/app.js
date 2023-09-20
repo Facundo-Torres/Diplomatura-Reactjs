103643a4412014d843c024ee87af20b0cd09411d
Facundo Torres <facutor@gmail.com>
2023-09-20 14:59:12 -0300

    node

--- Modulo-4-Node/app.js
+++ Modulo-4-Node/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var empresaRouter = require('./routes/empresa');
 
 var app = express();
 
@@ -21,6 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/empresa', empresaRouter);
+
+
+app.get('/casas', function(req, res){
+  res.send('Paginda de Casas')
+})
+app.get('/deptos', function(req, res){
+  res.send('Paginda de Deptos')
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Modulo-4-Node/routes/empresa.js
+++ Modulo-4-Node/routes/empresa.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Empresa. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Facundo Torres' });
+  res.send('Hola Empresa');
 });
 
 module.exports = router;
